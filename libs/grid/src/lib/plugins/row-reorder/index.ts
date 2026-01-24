b0b2a32a40335124fa2cc2db8abb415fb44d66d5
Oystein Amundsen <oystein.amundsen@gmail.com>
2026-01-24 17:28:00 +0100

    feat(row-reorder): Added row-reorder plugin

---
+++ libs/grid/src/lib/plugins/row-reorder/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Row Reorder Plugin Entry Point
+ * Re-exports plugin class and types for tree-shakeable imports.
+ *
+ * @module Plugins/Row-Reorder
+ */
+export { ROW_DRAG_HANDLE_FIELD, RowReorderPlugin } from './RowReorderPlugin';
+export type { PendingMove, RowMoveDetail, RowReorderConfig } from './types';
